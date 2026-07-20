c4062473ce53c61b5ff0b1fab6d954e843b8aca3
Sandeepmareeswaran <sandeepmaree2005@gmail.com>
2026-07-20 06:51:42 +0530

    explored tree

--- Tree/LC572.java
+++ Tree/LC572.java
@@ -14,33 +14,37 @@
  * }
  */
 class Solution {
-    public int minDepth(TreeNode root) {
-        
-        Queue<TreeNode> q = new LinkedList<>();
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        Queue<TreeNode> q=new LinkedList<>();
         if(root==null){
-            return 0;
+            return false;
         }
         q.offer(root);
-        int t=1;
         while(!q.isEmpty()){
             int s=q.size();
-            
             for(int i=0;i<s;i++){
-                TreeNode cur= q.poll();
+                TreeNode cur = q.poll();
+                if(check(cur,subRoot)){
+                    return true;
+                }
                 if(cur.left!=null){
                     q.offer(cur.left);
                 }
                 if(cur.right!=null){
                     q.offer(cur.right);
                 }
-                if(cur.left==null && cur.right==null){
-                    return t;
-                }
-                
             }
-            t++;
         }
-        return t;
+        return false;
         
+
+        
+    }
+    private boolean check(TreeNode root,TreeNode sub){
+        if(root==null && sub==null) return true;
+        if(root==null || sub==null) return false;
+        if(root.val != sub.val) return false;
+
+        return check(root.left,sub.left)&&check(root.right,sub.right);
     }
 }
